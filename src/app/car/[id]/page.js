ef0656eb18c2cf8deb2a1afbb63fb7ef3442b94f
Ishan Gupta <ishangupta3121@gmail.com>
2025-04-09 14:30:43 +0530

    Final before making dark mode changes

--- src/app/car/[id]/page.js
+++ src/app/car/[id]/page.js
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { useCars } from '../../context/CarContext';
+import { useCars } from '../../../context/CarContext';
 import { ArrowLeft, Sun, Moon } from 'lucide-react';
 import Link from 'next/link';
 
